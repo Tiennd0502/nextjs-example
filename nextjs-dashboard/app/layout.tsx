fc543d80460a4a34af6826c3164990450fd03981
Tien Nguyen <tien.nguyen@asnet.com.vn>
2023-12-20 09:51:57 +0700

    Initial structure for example

---
+++ nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/ui/global.css';
+
+interface RootLayoutProps {
+	children: React.ReactNode;
+}
+
+const RootLayout = ({ children }: RootLayoutProps) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
